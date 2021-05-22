c6e0b76183f150144c6fb0224b04012d201a77e1
Kentaro Yoshioka <meathouse47@gmail.com>
2021-05-22 09:25:06 +0900

    Update utils.py

--- mmdet/core/mask/utils.py
+++ mmdet/core/mask/utils.py
@@ -61,3 +61,29 @@ def encode_mask_results(mask_results):
         return encoded_mask_results, cls_mask_scores
     else:
         return encoded_mask_results
+    
+def raw_mask_results(mask_results):
+    """Encode bitmap mask to RLE code.
+
+    Args:
+        mask_results (list | tuple[list]): bitmap mask results.
+            In mask scoring rcnn, mask_results is a tuple of (segm_results,
+            segm_cls_score).
+
+    Returns:
+        list | tuple: RLE encoded mask.
+    """
+    if isinstance(mask_results, tuple):  # mask scoring
+        cls_segms, cls_mask_scores = mask_results
+    else:
+        cls_segms = mask_results
+    num_classes = len(cls_segms)
+    encoded_mask_results = [[] for _ in range(num_classes)]
+    for i in range(len(cls_segms)):
+        for cls_segm in cls_segms[i]:
+            encoded_mask_results[i].append(
+                    np.array(cls_segm[:, :, np.newaxis], dtype='uint8')[0])
+    if isinstance(mask_results, tuple):
+        return encoded_mask_results, cls_mask_scores
+    else:
+        return encoded_mask_results
