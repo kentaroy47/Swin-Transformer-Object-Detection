f5f59dbbd268e983b7234973945aabb1d7b5e4ca
Kentaro Yoshioka <meathouse47@gmail.com>
2021-05-22 09:36:38 +0900

    Update __init__.py

--- mmdet/core/mask/__init__.py
+++ mmdet/core/mask/__init__.py
@@ -1,6 +1,6 @@
 from .mask_target import mask_target
 from .structures import BaseInstanceMasks, BitmapMasks, PolygonMasks
-from .utils import encode_mask_results, split_combined_polys
+from .utils import encode_mask_results, split_combined_polys, raw_mask_results
 
 __all__ = [
     'split_combined_polys', 'mask_target', 'BaseInstanceMasks', 'BitmapMasks',
